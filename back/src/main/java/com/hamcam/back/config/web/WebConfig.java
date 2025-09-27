f41a4addeb6fdb26cdb824f2d5ff136d924f88c9
wovlf02 <onlygithub02@gmail.com>
2025-09-27 15:41:28 +0900

    ip 주소로 로그인 성공

--- back/src/main/java/com/hamcam/back/config/web/WebConfig.java
+++ back/src/main/java/com/hamcam/back/config/web/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.2:3000", "http://192.168.0.3:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
